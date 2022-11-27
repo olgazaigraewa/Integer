bf52a91a6211ae3be70899bf515b26d8e2481fcb
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-11-27 20:17:51 +0300

    ДЗ №2 Integer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class Main {
+
+    private final static Random random = new Random();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Set<Integer> number = new HashSet<>();
+        Set<Integer> numberRemuve = new HashSet<>();
+        for (int i = 1; i <= 20; i++) {
+            number.add(random.nextInt(1000));
+        }
+        for (Integer number1 : number) {
+            if (!(number1 % 2 == 0)) {
+                numberRemuve.add(number1);
+            } else {
+                System.out.print(number1 + " ");
+            }
+        }
+        for (Integer number1 : numberRemuve) {
+            number.remove(number1);
+        }
     }
 }
+
+
+
+
